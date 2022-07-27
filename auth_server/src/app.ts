3e7a396e7fec0f585f65fad4bf5de4b7c821aa5e
detronetdip <ayondip2001@gmail.com>
2022-07-27 22:46:59 +0530

    changed

--- auth_server/src/app.ts
+++ auth_server/src/app.ts
@@ -4,10 +4,14 @@ dotEnv.config({
 });
 import express from "express";
 import Database_Connection from "./config/DB";
+import cookie from "cookie-parser";
+import defaultRoute from "./routes/Default";
 
 Database_Connection;
 const app = express();
 const port = 3000;
+app.use(cookie());
+app.use(defaultRoute);
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
--- auth_server/src/config/DB.ts
+++ auth_server/src/config/DB.ts
@@ -1,6 +1,4 @@
 import mongoose from "mongoose";
-console.log(1);
-
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
