6fc5a065245e07c8987a82f6e82b557c6f893b29
detronetdip <ayondip2001@gmail.com>
2022-07-29 00:50:16 +0530

    edited

--- auth_server/src/routes/Default.ts
+++ auth_server/src/routes/Default.ts
@@ -2,6 +2,6 @@ import { Router, Request, Response } from "express";
 import { signToken } from "../utils/token";
 const route = Router();
 route.get("/", (req: Request, res: Response) => {
-  res.send(signToken({ name: "strsdfing", uid: "string", email: "string" }, 3000000));
+  res.send(signToken({ name: "strsdfing", uid: "string", email: "string" }, 420));
 });
 export default route;
--- auth_server/src/utils/token.ts
+++ auth_server/src/utils/token.ts
@@ -1,12 +1,21 @@
 import { UserDataToSign } from "../types";
 import jwt from "jsonwebtoken";
-import { getPrivateKey } from "./key";
+import { getPrivateKey, getPublicKey } from "./key";
 
 function signToken(userDetails: UserDataToSign, age: number): string {
   const token = jwt.sign(userDetails, getPrivateKey(), {
     algorithm: "RS256",
-    expiresIn: `${age}`,
+    expiresIn: age,
   });
-  return token; 
+  return token;
 }
-export { signToken };
+function verifyToken(token: string): string | boolean | object {
+  try {
+    jwt.verify(token, getPublicKey(), function (err, decoded) {
+      return decoded === undefined ? false : decoded;
+    });
+  } catch (error) {
+    return false;
+  }
+}
+export { signToken, verifyToken };
