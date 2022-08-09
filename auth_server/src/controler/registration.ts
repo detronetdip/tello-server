5ef88ff42b762ff7eb2db62bf7bfd16866607eae
detronetdip <ayondip2001@gmail.com>
2022-08-09 10:48:49 +0530

    fixed

--- auth_server/src/controler/registration.ts
+++ auth_server/src/controler/registration.ts
@@ -4,43 +4,46 @@ import { UserModel } from "../model/userModel";
 import bcrypt from "bcryptjs";
 import { v4 as uuidv4 } from "uuid";
 import { StatusCodes } from "../error_codes";
+import { ErrorMessages } from "../error_messages";
 
 export function handelRegistration(req: Request, res: Response) {
   const { email, username, password } = req.body;
   const salt = bcrypt.genSaltSync(10);
-  const addUser = async () => {
-    const USER = new UserModel({
-      userName: username,
-      email: email,
-      password: bcrypt.hashSync(password, salt),
-      uid: uuidv4(),
-    });
-    await USER.save()
-      .then((_e: object) => {
-        res.status(StatusCodes.Success).json({
-          msg: "Registration successfull",
-        });
-      })
-      .catch((e) => {
-        res.status(StatusCodes.BadRequest).json({
-          msg: "Duplicate Entry detected",
-          e,
-        });
-      });
-  };
   try {
     (async function () {
-      try {
-        await addUser();
-      } catch (er) {
-        console.log(er);
-      }
+      const addUser = async () => {
+        const USER = new UserModel({
+          userName: username,
+          email: email,
+          password: bcrypt.hashSync(password, salt),
+          uid: uuidv4(),
+        });
+        await USER.save()
+          .then((_e: object) => {
+            res.status(StatusCodes.Success).json({
+              msg: "Registration successfull",
+            });
+          })
+          .catch((e) => {
+            res.status(StatusCodes.BadRequest).json({
+              msg: "Duplicate Entry detected",
+              e,
+            });
+          });
+      };
+      addUser();
     })()
       .then((e) => console.log(e))
-      .catch((er) => console.log(er));
+      .catch((er) => {
+        res.status(StatusCodes.ServerError).json({
+          msg: ErrorMessages.ServerError,
+          code: StatusCodes.InternalServerError,
+        });
+      });
   } catch (error) {
-    res.status(StatusCodes.BadRequest).json({
-      msg: "Duplicate Entry detected",
+    res.status(StatusCodes.ServerError).json({
+      msg: ErrorMessages.ServerError,
+      code: StatusCodes.InternalServerError,
     });
   }
 }
