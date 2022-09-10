b95953af320d7b91e80d60c1d47699cd461c2375
detronetdip <ayondip2001@gmail.com>
2022-09-10 11:17:22 +0530

    startup edited

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
-sudo docker run --name mongodb -dti mongo
+sudo docker run --name mongodb -p 27017:27017 -dti mongo
+sudo docker run --name cache-store -p 6379:6379 -dti redis
 cd auth_server && npm start
