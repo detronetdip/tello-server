c39a3f1516f69af3c5ce8f54ab74e23a31e83857
detronetdip <ayondip2001@gmail.com>
2022-09-10 11:33:00 +0530

    restructures

---
+++ resource_server/error_messages/index.ts
@@ -0,0 +1,13 @@
+export enum ErrorMessages {
+  Successfull = "Successfull",
+  ServerError = "Server Error",
+  NotAuthenticate = "Not Authenticated.",
+  TokenExpired = "Token Expired.",
+  TokenVersionError = "Token Version Did Not Match.",
+  InsufficientData = "Insufficient Data",
+  MalformedData = "Invalid Data",
+  AllRedyPresent = "Alredy Present",
+  InvalidCredentials = "Invalid Credentials",
+  BadRequest = "Bad Request",
+  SomethingWentWrong = "Something went wrong",
+}
