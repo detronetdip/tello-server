42befb20bb0aa809fe7418b9813c2843a5608afb
detronetdip <ayondip2001@gmail.com>
2022-10-08 14:51:22 +0530

    updated

--- resource_server/src/graphql/resolvers/index.ts
+++ resource_server/src/graphql/resolvers/index.ts
@@ -1,9 +1,16 @@
 const resolvers = {
   Query: {
     // All queries will go here
+    hello: () => {
+      return "hello world";
+    },
   },
   Mutation: {
     // All mutations will go here
+    test: (_parrent, _args, _context, _info) => {
+      console.log(_info);
+      return "world";
+    },
   },
 };
 export default resolvers;
