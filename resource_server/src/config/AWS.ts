a83f64bffc843059857ea2750f1adcd444bb431b
detronetdip <ayondip2001@gmail.com>
2022-12-21 16:03:20 +0530

    image upload

---
+++ resource_server/src/config/AWS.ts
@@ -0,0 +1,3 @@
+import AWS from "aws-sdk";
+AWS.config.loadFromPath("./config.json");
+export default AWS;
--- resource_server/src/controller/index.ts
+++ resource_server/src/controller/index.ts
@@ -2,10 +2,12 @@ import { addFriend } from "./addFriend";
 import { userPost } from "./Post";
 import { accept } from "./accept";
 import { block } from "./block";
+import { UploadFile } from "./UploadFile";
 
-export const Controller={
-    addFriend,
-    accept,
-    userPost,
-    block
-}
+export const Controller = {
+  addFriend,
+  accept,
+  userPost,
+  block,
+  UploadFile,
+};
