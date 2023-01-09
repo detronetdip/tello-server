7906717fedc8741197f276a760480b199fde2778
detronetdip <ayondip2001@gmail.com>
2023-01-09 11:17:47 +0530

    change password template added

--- notification_service/src/utils/mail/MailConstruct.ts
+++ notification_service/src/utils/mail/MailConstruct.ts
@@ -1,10 +1,17 @@
-import { loginTemplate, otpTemplate } from "./mail-templates/template";
+import {
+  changePasswordNotification,
+  loginTemplate,
+  otpTemplate,
+} from "./mail-templates/template";
 import crypto from "crypto";
 export function constructMail(
-  category: "OTP" | "REGISTRATION"
+  category: "OTP" | "REGISTRATION" | "PASSWORDCHANGED",
+  _aditional: { aditional?: object }
 ) {
   if (category === "OTP") {
     return otpTemplate(crypto.randomBytes(6).toString("hex"));
+  } else if (category === "PASSWORDCHANGED") {
+    return changePasswordNotification(crypto.randomBytes(6).toString("hex"));
   } else {
     return loginTemplate();
   }
