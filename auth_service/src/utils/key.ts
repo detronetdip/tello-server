3c3d41d4ee2861b4b97f025a67276b0c6dd02841
detronetdip <ayondip2001@gmail.com>
2023-02-11 19:46:33 +0530

    path added

--- auth_service/src/utils/key.ts
+++ auth_service/src/utils/key.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from "fs";
 import path from "path";
 export function getPrivateKey(): string {
-  const Path = path.resolve(__dirname, "key");
-  return readFileSync(Path+"private.pem", "utf8");
+  const Path = path.resolve(__dirname, "..", "key");
+  return readFileSync(Path + "/private.pem", "utf8");
 }
 export function getPublicKey(): string {
-  const Path = path.resolve(__dirname, "key");
-  return readFileSync(Path + "public.pem", "utf8");
+  const Path = path.resolve(__dirname, "..", "key");
+  return readFileSync(Path + "/public.pem", "utf8");
 }
